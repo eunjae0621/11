3f8cc09e7274f8e1c8b8775ab9c88c80a5fd93eb
eunjae0621 <chaej@sookmyung.ac.kr>
2025-11-13 15:43:40 +0900

    06

--- main.c
+++ main.c
@@ -4,20 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int	i;
-	int sum = 0;
-	int grade[5];
+	int	i = 100;
+	int *p = &i;
+	int **q = &p;
 	
-	for(i=0; i<5; i++) {
-		printf("grade[%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
+	*p = 200;
+	printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
 	
-	for (i=0; i<5; i++) {
-		printf("grade[%i] = %i\n", i, *(grade+i));
-		sum = sum + *(grade+i);
-	}
-	
-	printf("Average : %i\n", sum/5);
+	**q = 300;
+	printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
 	return 0;
 }
